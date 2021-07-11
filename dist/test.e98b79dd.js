e5f25789abe6965267cf178d380930328c66ada7
winter <1304879929@qq.com>
2021-07-11 22:20:47 +0800

    JS 部分，手写代码

--- dist/test.e98b79dd.js
+++ dist/test.e98b79dd.js
@@ -117,79 +117,67 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"css.js":[function(require,module,exports) {
+"use strict";
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var string = "/*\n* \u9996\u5148\uFF0C\u9700\u8981\u753B\u718A\u672C\u718A\u7684\u8EAB\u4F53\n*/\n  .body {\n    width: 320px;\n    height: 240px;\n    background-color: black;\n  }\n  /*\n  * \u63A5\u4E0B\u6765\uFF0C\u753B\u718A\u672C\u718A\u7684\u773C\u775B\n  */\n  .eye {\n    border-radius: 50%;\n    width: 64px;\n    height: 64px;\n    background-color: white;\n  }\n  /*\n  * \u53F3\u773C\u5728\u53F3\u8FB9\uFF08\u5E9F\u8BDD\uFF09\n  */\n  .eye.right {\n    transform: translateX(70px);\n  }\n  /*\n  * \u5DE6\u773C\u5728\u5DE6\u8FB9\uFF08\u5E9F\u8BDD\uFF09\n  */\n  .eye.left {\n    transform: translateX(-70px);\n  }\n  /*\n  * \u7136\u540E\u52A0\u4E24\u4E2A\u773C\u73E0\u5B50\uFF0C\u5E76\u8BA9\u5B83\u52A8\u8D77\u6765\n  */\n  .eye::before { \n    height: 24px;\n    width: 10px;\n    background-color: black;\n    animation: blink 0.7s infinite;\n  }\n/*\n* \u753B\u7709\u6BDB\n*/\n  .eyebrow {\n    width: 36px;\n    height: 36px;\n    background-color: white;\n    \n  }\n  .eyebrow::before {   \n    width: 36px;\n    height: 36px;\n    background-color: black;\n  }\n  /*\n* \u628A\u7709\u6BDB\u653E\u5230\u6B63\u786E\u7684\u4F4D\u7F6E\n*/\n  .eyebrow.right {\n    transform: translateX(80px) rotate(18deg);\n  }\n  .eyebrow.left {\n    transform: translateX(-80px) rotate(-18deg);\n  }\n  /* \n * \u518D\u753B\u718A\u672C\u718A\u7684\u5C0F\u8138\u86CB\n  */\n  .face {\n    width: 80px;\n    height: 80px;\n    background-color: #ef2b2d;\n    position: absolute;\n  }\n  /*\n* \u628A\u5C0F\u8138\u86CB\u653E\u5230\u6B63\u786E\u7684\u4F4D\u7F6E\n*/\n  .face.right {\n    transform: translateX(140px);\n  }\n  .face.left {\n    transform: translateX(-140px);\n  }\n  /*\n  * \u753B\u718A\u672C\u718A\u7684\u62DB\u98CE\u8033\n  */\n  .ear {\n    width: 60px;\n    height: 60px;\n    background-color: black;\n    \n  }\n  /*\n  * \u628A\u8033\u6735\u653E\u5230\u6B63\u786E\u7684\u4F4D\u7F6E\n  */\n  .ear.right {\n    transform: translateX(130px);\n  }\n  .ear.left {\n    transform: translateX(-130px);\n  }\n  .ear::before {\n    width: 38px;\n    height: 38px;\n    background-color: white;\n   \n  }\n  .ear.right::before {\n    transform: translateX(-3px);\n  }\n  .ear.left::before {\n    transform: translateX(3px);\n  }\n  /* \n  * \u6700\u540E\uFF0C\u753B\u718A\u672C\u718A\u7684\u9F3B\u5B50\u548C\u5634\u5DF4\n  */\n  .noseAndMouse {\n    width: 160px;\n    height: 120px;\n    background-color: white;\n  }\n  .noseAndMouse .nose {\n    width: 50px;\n    height: 36px;\n    background-color: black;\n  }\n  .noseAndMouse .mouse {\n    width: 140px;\n    height: 30px;\n    background-color: black;\n  }\n  /*\n  * \u597D\u4E86\uFF0C\u8FD9\u53EA\u718A\u672C\u718A\u9001\u7ED9\u4F60\u5566\uFF01\n  */\n  ";
+var _default = string;
+exports.default = _default;
+},{}],"test.js":[function(require,module,exports) {
+"use strict";
+
+var _css = _interopRequireDefault(require("./css.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// debugger
+console.log(_css.default);
+var n = 1;
+var code = document.querySelector('#code');
+var style = document.querySelector('#style');
+console.log(code);
+code.innerText = _css.default.substr(0, n);
+style.innerHTML = _css.default.substr(0, n);
+console.log(n);
+
+var timeCode = function timeCode() {
+  n += 1;
+
+  if (n > _css.default.length) {
+    window.clearInterval(id);
   }
 
-  return bundleURL;
-}
+  code.innerText = _css.default.substr(0, n);
+  style.innerHTML = _css.default.substr(0, n); // console.log(n)
 
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+  code.scrollTop = code.scrollHeight;
+};
 
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
+var time = 50;
+var id = setInterval(function () {
+  timeCode();
+}, time);
 
-  return '/';
-}
+btnSlow.onclick = function () {
+  window.clearInterval(id);
+  time = 100;
+  id = setInterval(function () {
+    timeCode();
+  }, time);
+};
 
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/bundle-url.js"}],"style.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/css-loader.js"}],"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+btnFast.onclick = function () {
+  window.clearInterval(id);
+  time = 10;
+  id = setInterval(function () {
+    timeCode();
+  }, time);
+};
+},{"./css.js":"css.js"}],"C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +205,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63336" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54424" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -392,5 +380,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/style.e308ff8e.js.map
+},{}]},{},["C:/Users/Administrator/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","test.js"], null)
+//# sourceMappingURL=/test.e98b79dd.js.map
